59cedd42374ffb3262f82010b549421f3d9fe2e9
Demonic-Mailbox <96639973+Demonic-Mailbox@users.noreply.github.com>
2021-12-24 21:18:17 -0500

    Update DO YOU LIKE BEANS???!!11.py

--- DO YOU LIKE BEANS???!!11.py
+++ DO YOU LIKE BEANS???!!11.py
@@ -1,6 +1,4 @@
 import random
-print("Welcome to the random numner generator!! (1-100)")
-print("this is like the first project i made without a tutorial so its simple")
 randNumb = random.randrange(1, 100)
 print(randNumb)
 if randNumb == 69:
